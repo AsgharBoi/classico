9376a5792c78c2ef5f9874ded6ae085e59d22b64
AsgharBoi <sz07201@st.habib.edu.pk>
2023-06-21 07:21:12 +0500

    Day4 Complete

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -6,16 +6,64 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20.0, child: Text("Bruh")),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  vertical: 16.0, horizontal: 32.0), //all => Symmetry padding
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true, // tells that text shouldnt be shown
+                    decoration: InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("Ayo!");
+                    },
+                    child: Text("Login"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
+
+      // child: Center(
+      //   child: Text(
+      //     "Login Page",
+      //     style: TextStyle(
+      //       fontSize: 20,
+      //       color: Colors.blue,
+      //       fontWeight: FontWeight.bold,
+      //     ),
+      //   ),
+      // ),
